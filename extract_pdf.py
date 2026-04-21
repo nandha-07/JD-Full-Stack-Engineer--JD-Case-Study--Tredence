7a1095ecc1c7c9a46a2e6bf55260ed3da99a417a
Nandha Kumar K <nandhakumar.k7071@gmail.com>
2026-04-21 09:58:33 +0530

    Delete extract_pdf.py

--- extract_pdf.py
+++
@@ -1,8 +0,0 @@
-import pdfplumber
-
-with pdfplumber.open("JD Full Stack Engineer -JD+ Case Study -Tredence.pdf") as pdf:
-    for i, page in enumerate(pdf.pages):
-        print(f"\n--- PAGE {i+1} ---")
-        text = page.extract_text()
-        if text:
-            print(text)
